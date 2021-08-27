1b74333021008a740be4975eb53377eb0b6fa89f
SingularAI <jlupker@uwo.ca>
2021-08-27 14:49:47 -0400

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,10 +1,12 @@
 <?php
-if (isset($_POST['submit'])) {
+if(isset($_POST['submit'])){
     $to = "lupkerconsulting@gmail.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $subject = "Inquiry for SingularAI";
     $message = $_POST['message'];
-    mail($to, $subject, $message, $from);
+
+    $headers = "From:" . $from;
+    mail($to,$subject,$message,$headers);
     echo "Email Sent, We Will Contact You Shortly. Thanks!";
 }
 ?>
